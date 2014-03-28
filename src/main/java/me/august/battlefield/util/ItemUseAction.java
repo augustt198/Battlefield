6712210c18e61d38033a793b8a8eeda1ba22f6b3
August <augustt198@gmail.com>
2014-03-27 20:35:04 -0400

    Also make item use actions specific to player

--- src/main/java/me/august/battlefield/util/ItemUseAction.java
+++ src/main/java/me/august/battlefield/util/ItemUseAction.java
@@ -1,12 +1,14 @@
 package me.august.battlefield.util;
 
 import me.august.battlefield.BattlefieldPlugin;
+import me.august.battlefield.player.BattlefieldPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,26 +17,40 @@ import java.util.List;
  */
 public class ItemUseAction implements Listener {
 
+	private static List<ItemUseAction> itemActions = new ArrayList<>();
+
+	public static void removeByPlayer(BattlefieldPlayer bfPlayer) {
+		for(ItemUseAction action : itemActions) {
+			if(action.getPlayer() == bfPlayer) {
+				BattlefieldPlugin.unregisterListener(action);
+				itemActions.remove(action);
+			}
+		}
+	}
+
 	private List<Action> actions;
 	private ItemStack item;
 	private Runnable run;
 	private boolean cancel;
+	private BattlefieldPlayer player;
 
-	public ItemUseAction(ItemStack item, Runnable run, Action... actions) {
-		this(item, run, false, actions);
+	public ItemUseAction(ItemStack item, BattlefieldPlayer player, Runnable run, Action... actions) {
+		this(item, player, run, false, actions);
 	}
 
-	public ItemUseAction(ItemStack item, Runnable run, boolean cancel, Action... actions) {
+	public ItemUseAction(ItemStack item, BattlefieldPlayer player, Runnable run, boolean cancel, Action... actions) {
 		this.item = item;
+		this.player = player;
 		this.run = run;
 		this.actions = Arrays.asList(actions);
 		this.cancel = cancel;
 		BattlefieldPlugin.registerListener(this);
+		itemActions.add(this);
 	}
 
 	@EventHandler
 	public void inventoryClickEvent(PlayerInteractEvent event) {
-		if(event.getPlayer().getItemInHand().equals(item) && actions.contains(event.getAction())) {
+		if(event.getPlayer() == player.getPlayer() && event.getPlayer().getItemInHand().equals(item) && actions.contains(event.getAction())) {
 			run.run();
 			event.setCancelled(cancel);
 		}
@@ -52,4 +68,7 @@ public class ItemUseAction implements Listener {
 		return run;
 	}
 
+	public BattlefieldPlayer getPlayer() {
+		return player;
+	}
 }
